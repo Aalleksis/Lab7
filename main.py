2c567515f3b4488b62d87099ae2600e8b7742366
Aalleksis <leshamylchenkol24@gmail.com>
2025-11-26 17:56:43 +0300

    main.py

--- main.py
+++ main.py
@@ -1,55 +1,69 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time as tm
 from mpl_toolkits.mplot3d import Axes3D
-
-
-def start():
-    arr = np.array([5, 9, 10, 'ad', 7])
-    arr = arr[::-1]
-    print(arr)
-
-
-def tableLoad():
-    arr = np.genfromtxt('voltage.csv', delimiter=',')
-    time = arr[:100,0]
-    time = time[:,np.newaxis]
-    curr = arr[:100,1]
-    curr = curr[:,np.newaxis]
-    volt = arr[:100,2]
-    volt = volt[:,np.newaxis]
-
-    plt.plot(time, curr * 50, 'b', time, volt, 'r')
-    plt.show()
-
-
-def hist():
-    arr = np.genfromtxt('test.csv', delimiter=',')
+import random as rd
+#Каждый метод есть в функции start на строке 77
+#Я взял вариант № 2
+##Задание №1, проверка работы numpy
+def numPyTest():
+    nparray = np.random.randint(1,100,size=2000000)
+    pyarray = [rd.randint(1,100) for i in range(2000000)]
+    #
+    nA,nB = [nparray[:1000000], nparray[1000000:]]
+    pA,pB = [pyarray[:1000000], pyarray[1000000:]]
+    #
+    def timer(func,a,b):
+        time = tm.perf_counter()
+        func(a,b)
+        return tm.perf_counter() - time
+    def multiplyarray(a,b):
+        return [a[i] * b[i] for i in range(len(a))]
+    #
+    timeN = timer(np.multiply, nA,nB)
+    timeP =  timer(multiplyarray,pA,pB)
+    print(f"Время умножения в NumPy: {timeN}")
+    print(f'Время умножения в python:{timeP}')
+    print(f'Numpy быстрее стандарта в {round(timeP/timeN)} раза')
+#Задание №2, построение гистограммы 
+def myHist():
+    arr = np.genfromtxt('data2.csv', delimiter=',')
     arr = arr[1:]
-    daysInYear = 365.25
-
-    age = np.int_(arr[:,1] / daysInYear)
-
+    pH = (arr[:,0])
     fig = plt.figure(figsize=(6, 4))
     ax = fig.add_subplot()
-    ax.hist(age, 100, (50, 60))
+    ax.set_xlabel('pH')
+    ax.set_ylabel('Частота')
+    ax.set_title(f'гистограмма для параметра pH \n среднеквадратичное отклонение:{np.std(np.nan_to_num(pH))}')
+    ax.hist(pH, 16)
     ax.grid()
     plt.show()
+#Задание №3, построение функции в 3D
+def drow3D_function():
+      x_value = np.linspace(-2*np.pi,2*np.pi, 100)
+      y_value = np.sin(x_value)*np.cos(x_value)
+      z_value = np.sin(x_value)*np.cos(x_value)
+      frame = plt.figure()
+      ax = frame.add_subplot(111, projection='3d')
+      ax.plot(x_value, y_value, z_value, marker='x', c='red')
+      plt.show()          
 
+def Start():
+#Задание 1
 
-def plot3d():
-    np.random.seed(40)
-    xs = np.linspace(0, 10, 20)
-    ys = xs
-    zs = np.sin(xs)
+    #numPyTest()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot(xs, ys, zs, marker='x', c='red')
-    plt.show()
+#Задание 2
 
+    #myHist()
 
-if __name__ == '__main__':
-    #start()
-    #tableLoad()
-    #hist()
-    plot3d()
+#Задание 3
+
+    #drow3D_function()
+
+#Доп задание(в 2 вариантах)
+
+    #move_graph_version1()
+    #move_graph_version2()
+    pass
+Start()
